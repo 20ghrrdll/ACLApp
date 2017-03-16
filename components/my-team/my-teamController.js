0d6b68bba7d8aab264e47957b955e9989badf656
20ghrrdll <caraha@stanford.edu>
2017-03-16 01:38:50 -0700

    Got team to be organized and aesthetically pleasing.

--- components/my-team/my-teamController.js
+++ components/my-team/my-teamController.js
@@ -2,37 +2,47 @@
 
 cs142App.controller('MyTeamController', ['$scope',
     function ($scope) {
+    	$scope.userInfo = {};
+    	$scope.userInfo.photos = [
+			"Melissa.jpg",
+			"Allan.jpeg",
+			"Annabel.jpg",
+			"Debnil.png",
+			"Divya.jpg",
+			"Elizabeth.jpg",
+		];
 
     }]);
 
-cs142App.run(['$rootScope', function($rootScope){
-  $rootScope.selectedHour = 12;
-}]);
 
-cs142App.directive('clock', ['$transform',function($transform) {
+
+cs142App.directive('huddle', ['$transform',function($transform) {
   return {
     restrict: 'E',
     scope: {
       model: "=ngModel"  
     },
     compile: function(clockElem) {
-      var $hour, $hand, $hours = [];
+      var $hour, $hours = [];
+      var photos = [
+			"Melissa.jpg",
+			"Kayland.jpeg",
+			"Annabel.jpg",
+			"Debnil.png",
+			"Divya.jpg",
+			"Elizabeth.jpg",
+			"Allan.jpeg",
+			"Avatar.png",
+
+		];
       
-      for (var h = 1; h <= 12; h++) {
-        $hour = angular.element('<div class="clock-hour">' + h + '</div>');
+      for (var h = 1; h <= 8; h++) {
+        $hour = angular.element('<div class="teammate"><img ng-src="../images/'+ photos[h-1]+'" class="teammateImage"></div>');
         clockElem.append($hour);
         $transform.set($hour,
-          'rotate(' + (30 * h - 90) + 'deg) translateX(100px) rotate(' + (-30 * h + 90) + 'deg)');
+          'rotate(' + (45 * h - 90) + 'deg) translateX(275px) rotate(' + (-45 * h + 90) + 'deg)');
         $hours.push($hour);
       }
-      $hand = angular.element('<div class="clock-hand"></div>');
-      clockElem.append($hand);
-
-      var centerHandToHour = function(hour) {
-        $transform.set($hand, 
-          'rotate(' + (30 * hour - 90) + 'deg) translateX(100px)');
-      };
-
     }
   };
 }]);
