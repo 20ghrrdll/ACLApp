9aa9e36567c14e30ed6ad9e48185bbcaed5f8044
20ghrrdll <caraha@stanford.edu>
2017-03-15 23:19:17 -0700

    added icon to nav bar

--- components/my-team/my-teamController.js
+++ components/my-team/my-teamController.js
@@ -2,6 +2,50 @@
 
 cs142App.controller('MyTeamController', ['$scope',
     function ($scope) {
-        
+
     }]);
+
+/*cs142App.run(['$rootScope', function($rootScope){
+  $rootScope.selectedHour = 12;
+}]);
+
+cs142App.directive('clock', ['$transformProvider',function($transform) {
+  return {
+    restrict: 'E',
+    scope: {
+      model: "=ngModel"  
+    },
+    compile: function(clockElem) {
+      var $hour, $hand, $hours = [];
+      
+      for (var h = 1; h <= 12; h++) {
+        $hour = angular.element('<div class="clock-hour">' + h + '</div>');
+        clockElem.append($hour);
+        $transform.set($hour,
+          'rotate(' + (30 * h - 90) + 'deg) translateX(100px) rotate(' + (-30 * h + 90) + 'deg)');
+        $hours.push($hour);
+      }
+      $hand = angular.element('<div class="clock-hand"></div>');
+      clockElem.append($hand);
+
+      var centerHandToHour = function(hour) {
+        $transform.set($hand, 
+          'rotate(' + (30 * hour - 90) + 'deg) translateX(100px)');
+      };
+
+      return function postLink(scope, element) {
+        var dragging = false;
+
+        var unwatch = scope.$watch('model', function(h) {
+          if (!dragging) { centerHandToHour(h); }
+        });
+
+        element.on('$destroy', function() {
+          unwatch();
+        });
+       
+      }
+    }
+  };
+}]);*/
 
